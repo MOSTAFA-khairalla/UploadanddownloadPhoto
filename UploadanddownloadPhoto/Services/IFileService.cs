32b1818d627443ae7d88e5aa07874a7e27b7e397
Elhamd Pc <Elhamd Pc@DESKTOP-NB8KSO3>
2022-08-22 17:35:01 +0200

    Add project files.

---
+++ UploadanddownloadPhoto/Services/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace UploadanddownloadPhoto.Services
+{
+    public interface IFileService
+    {
+
+            void UploadFile(List<IFormFile> files, string subDirectory);
+            (string fileType, byte[] archiveData, string archiveName) DownloadFiles(string subDirectory);
+            string SizeConverter(long bytes);
+      
+    }
+}
